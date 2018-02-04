06283a247096d19399adcd234ae6c03eeeaf011c
Sohail Rajdev <sohailrajdev97@gmail.com>
2018-02-04 16:36:40 +0530

    schemas: Add members schema

---
+++ schemas/members.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var membersSchema = new Schema({
+    name: String,
+    github: String,
+    tagline: String,
+    description: String,
+    skills: [String],
+    email: {
+    	type: String,
+    	default: ""
+    },
+    phone: {
+    	type: String,
+    	default: ""
+    }
+});
+
+var model = mongoose.model('members', membersSchema);
+
+module.exports = model;
